27671a284645b152bc226cc682d9e7ef0bea1126
Fasha18 <fbiaye18@gmail.com>
2026-06-15 23:48:20 +0100

    Fix database config for Railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,26 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASS,
-    {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+      }
+    );
 
 module.exports = sequelize;
